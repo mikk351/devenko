120bd6f15a0a99ef930492b1fa279f76485550af
Mikk Õun <mikk351@gmail.com>
2019-02-18 16:54:05 +0200

    Update archer.sh

--- archer.sh
+++ archer.sh
@@ -1,10 +1,14 @@
-sudo pacman -S chromium flatpak code atom docker nodejs npm yaourt xclip --noconfirm
+sudo pacman -S chromium flameshot code atom docker nodejs npm yaourt xclip --noconfirm
 
+# install? flatpak
 yaourt -S qdigidoc4 chrome-token-signing --noconfirm
 yaourt -S spotify sublime-text-dev pulseaudio-dlna --noconfirm
 
 sudo systemctl enable pcscd
 sudo systemctl start pcscd
+
+sudo systemctl enable docker
+sudo systemctl start docker
 
 sudo pip install docker-compose
 sudo npm install -g @angular/cli http-server
