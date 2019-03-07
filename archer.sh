d53260e7a20e30503066bbc0c598fce7bc1e7311
Mikk Õun <mikk351@gmail.com>
2019-03-07 10:17:23 +0200

    pippi

--- archer.sh
+++ archer.sh
@@ -1,14 +1,21 @@
-sudo pacman -S chromium flameshot code atom docker nodejs npm yaourt xclip --noconfirm
+sudo pacman -S chromium flameshot code atom docker nodejs npm yaourt xclip python-pip --noconfirm
 
 # install? flatpak
-yaourt -S qdigidoc4 chrome-token-signing --noconfirm
-yaourt -S spotify sublime-text-dev pulseaudio-dlna --noconfirm
+yaourt -S spotify sublime-text-dev --noconfirm
+#yaourt -S pulseaudio-dlna --noconfirm
 
-sudo systemctl enable pcscd
-sudo systemctl start pcscd
+# SmartCard
+yaourt -S qdigidoc4 chrome-token-signing --noconfirm
+sudo systemctl enable pcscd && sudo systemctl start pcscd
+
 
 sudo systemctl enable docker
 sudo systemctl start docker
 
-sudo pip install docker-compose
-sudo npm install -g @angular/cli http-server
+# Install for one user
+pip install --user docker-compose
+# Add user bin to path $HOME/.local/bin
+printf '#!/bin/bash\nPATH=$PATH:$HOME/.local/bin\n' | sudo tee -a /etc/profile.d/custom.sh > /dev/null
+
+
+sudo npm install -g @angular/cli @vue/cli http-server
