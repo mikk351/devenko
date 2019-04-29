3d814b565ef26ea3311c81f666ef14105ba48d49
Mikk Õun <mikk351@gmail.com>
2019-04-29 22:42:12 +0300

    Update archer.sh

--- archer.sh
+++ archer.sh
@@ -21,6 +21,7 @@ npm config set prefix '~/.npm-global'
 # Add user bin to path $HOME/.local/bin
 printf '#!/bin/bash\nPATH=$PATH:$HOME/.local/bin:$HOME/.npm-global\n' | sudo tee -a /etc/profile.d/custom.sh > /dev/null
 source /etc/profile.d/custom.sh
+# sudo chmod a+x /etc/profile.d/ # ubuntus
 
 # For fish
 #printf '\nset -gx PATH $PATH:$HOME/.local/bin:$HOME/.npm-global/bin\n' | tee -a ~/.config/fish/config.fish > /dev/null
